19c7b30776518a64c76b90bcc88a9e8a1dc07e00
桐原知空 <42866646+kiriharachikuu@users.noreply.github.com>
2024-04-19 15:29:15 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'index.html'),
+        news: path.resolve(__dirname, 'News-Center.html'),
+        media: path.resolve(__dirname, 'Media-Center.html'),
+        time: path.resolve(__dirname, 'Time-Corridor.html'),
+        about: path.resolve(__dirname, 'About-Us.html'),
+      }
+    }
+  }
 })
+
+
